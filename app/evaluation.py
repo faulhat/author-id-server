1d7955fcc0db270aaca9dd59240a9e0b3b339b6c
Thomas Faulhaber <thomasihs365@gmail.com>
2022-03-28 10:55:45 -0700

    Changed library for image processing (from sqlalchemy-imageattach to sqlalchemy-media)

--- app/evaluation.py
+++ app/evaluation.py
@@ -1,6 +1,14 @@
-from re import template
-from flask import Blueprint, Response
+"""
+    Thomas: This module contains the app views responsible for interaction with the fingerprinting
+    model.
+"""
 
+import requests
+
+from flask import Blueprint, Response
+from flask_login import login_required
+
+from .models import UserEval
 from .forms import LabelledSampleForm, UnlabelledSampleForm
 from .main import settings
 
@@ -10,8 +18,16 @@ evalviews = Blueprint("evalviews", __name__, template_folder="templates/")
 
 # Add a new labelled image.
 @evalviews.route("/new")
-def new_sample():
+@login_required
+def new_sample() -> Response:
     form = LabelledSampleForm()
 
     if form.validate_on_submit():
-        pass
+        query_url = settings["modelServerIP"] + ":" + \
+            settings["modelServerPort"] + "/eval"
+        post_data = {"file": form.attachment.data}
+        res = requests.post(query_url, post_data)
+        res_data = res.json()
+
+        new_eval = UserEval(name=form.name.data, attachment=form.attachment.data)
+
--- app/models.py
+++ app/models.py
@@ -5,10 +5,30 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from sqlalchemy_imageattach.entity import image_attachment, Image
+from sqlalchemy_media import Image, ImageAnalyzer, ImageProcessor, ImageValidator
+
+from .main import settings
 
 
+TEMP_PATH = settings["tempdir"]
 db = SQLAlchemy()
+
+
+class Sample(Image):
+    __pre_processors__ = [
+        ImageAnalyzer(),
+        ImageValidator(
+            minimum=(560, 400),
+            maximum=(2240, 1600),
+            min_aspect_ratio=1.0,
+            content_types=['image/jpeg', 'image/png']
+        ),
+        ImageProcessor(
+            fmt='jpeg',
+            width=1120,
+            height=800,
+        )
+    ]
 
 
 class User(UserMixin, db.Model):
@@ -18,15 +38,22 @@ class User(UserMixin, db.Model):
     email = db.Column(db.Text, unique=True, nullable=False)
     name = db.Column(db.Text, nullable=False)
     pw_hash = db.Column(db.String(100), nullable=False)
-    evals = image_attachment("UserEval")
+    evals = db.relationship("SampleEval", back_populates="user")
 
 
-class UserEval(db.Model, Image):
-    __tablename__ = "user_eval"
+class SampleEval(db.Model):
+    __tablename__ = "sample_eval"
     
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-    user = db.relationship("User")
-    eval_array = db.Column(db.PickleType, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User", back_populates="evals")
 
     # Name of this handwriting sample's known author
     name = db.Column(db.Text, nullable=False)
+
+    # Image for the sample
+    image = db.Column(Sample.as_mutable(db.Json), nullable=False)
+
+    # Fingerprint of the sample
+    eval_array = db.Column(db.Json, nullable=False)
+
